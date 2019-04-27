6a67c75f1f0b219f56b929969db2348b48c71431
sam-malanchuk <malanchuksam@gmail.com>
2019-04-27 13:08:37 -0400

    completed Callbacks in functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,20 +6,23 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+const consume = (parm1, parm2, cb) => cb(parm1, parm2);
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+const add = (parm1, parm2) => parm1 + parm2;
+const multiply = (parm1, parm2) => parm1 * parm2;
+const greeting = (first, last) => `Hello ${first} ${last}, nice to meet you!`;
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
+// was told to 'return' not 'console.log' the parameters in the functions so adjusting invokes to contain 'console.log'
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ==== 
 
